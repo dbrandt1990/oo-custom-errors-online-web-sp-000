88c1db33d977ed8bfd19e6643383205addb4e372
Danny Brandt <dvbrandt90@gmail.com>
2021-01-14 20:17:52 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -9,7 +9,7 @@ class Person
     self.partner = person
     if person.class != Person 
       raise ParentError
-      rescue ParentError
+      rescue "Hello"
     end
     else
     person.partner = self
